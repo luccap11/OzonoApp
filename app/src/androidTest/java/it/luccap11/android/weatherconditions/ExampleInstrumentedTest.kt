4b10a3e9e248cdac5cca6975b182a4124dc892bf
Luca Capitoli <capitoli@skypointplanetariums.eu>
2020-12-21 09:42:12 +0100

    refactoring

--- app/src/androidTest/java/it/luccap11/android/weatherconditions/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/it/luccap11/android/weatherconditions/ExampleInstrumentedTest.kt
@@ -1,8 +1,8 @@
 package it.luccap11.android.weatherconditions
 
-import android.support.test.InstrumentationRegistry
-import android.support.test.runner.AndroidJUnit4
 
+import androidx.test.InstrumentationRegistry
+import androidx.test.runner.AndroidJUnit4
 import org.junit.Test
 import org.junit.runner.RunWith
 
