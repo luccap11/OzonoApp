0af5e8f1519ca1ebbc166708d2a429451b116434
Luca Capitoli <capitoli@skypointplanetariums.eu>
2020-12-22 09:11:00 +0100

    refactoring

--- app/src/main/java/it/luccap11/android/weatherconditions/model/WeatherViewModel.kt
+++ app/src/main/java/it/luccap11/android/weatherconditions/model/WeatherViewModel.kt
@@ -7,6 +7,7 @@ import androidx.lifecycle.*
 import com.android.volley.Request
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import it.luccap11.android.weatherconditions.R
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
 
@@ -19,11 +20,11 @@ class WeatherViewModel(application: Application) : AndroidViewModel(application)
 
     fun getData(selectedCity: String) {
         liveData.postValue(Resource.Loading())
-        if (!selectedCity.isBlank()) {
+        if (selectedCity.isNotBlank()) {
             viewModelScope.launch(Dispatchers.IO) {
-                val url =
-                    "https://api.openweathermap.org/data/2.5/forecast?q=" + selectedCity + "&APPID=7d85604d75067f7a0da53ac8f70c5364"
-//
+                val resource = getApplication<Application>().resources
+                val url = String.format("https://api.openweathermap.org/data/2.5/forecast?q=%s&APPID=%s", selectedCity,
+                    resource.getString(R.string.owm_api_key))
                 val requestQueue = Volley.newRequestQueue(getApplication())
                 val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
                     val city = response!!.getJSONObject("city").getString("name")
@@ -40,7 +41,7 @@ class WeatherViewModel(application: Application) : AndroidViewModel(application)
                     liveData.postValue(Resource.Success(result))
                     }) { error ->
                         Log.e("TAG", error.toString())
-                        liveData.postValue(Resource.Error("Nothing to display"))
+                        liveData.postValue(Resource.Error(resource.getString(R.string.error_label)))
                     }
 
                 requestQueue.add(jsonObjectRequest)
