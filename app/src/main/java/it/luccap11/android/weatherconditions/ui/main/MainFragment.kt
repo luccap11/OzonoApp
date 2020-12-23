5fcdd1b37103b6a651ea0f8bec64fe5224619efe
Luca Capitoli <capitoli@skypointplanetariums.eu>
2020-12-23 17:12:11 +0100

    added simple instrumented tests

--- app/src/main/java/it/luccap11/android/weatherconditions/ui/main/MainFragment.kt
+++ app/src/main/java/it/luccap11/android/weatherconditions/ui/main/MainFragment.kt
@@ -8,7 +8,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.*
 import androidx.annotation.Nullable
-import androidx.appcompat.widget.SearchView
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
