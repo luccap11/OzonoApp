4299f47661a56a185a0e764a56568c8c25cb9a34
Luca Capitoli <capitoli@skypointplanetariums.eu>
2020-12-23 08:34:16 +0100

    changed the variables from var to val

--- app/src/main/java/it/luccap11/android/weatherconditions/model/data/WeatherData.kt
+++ app/src/main/java/it/luccap11/android/weatherconditions/model/data/WeatherData.kt
@@ -4,10 +4,10 @@ package it.luccap11.android.weatherconditions.model.data
  * @author Luca Capitoli
  */
 data class WeatherData(
-    var descr: String,
-    var temp: Float,
-    var date: String,
-    var icon: String
+    val descr: String,
+    val temp: Float,
+    val date: String,
+    val icon: String
 ) {
 
 }
