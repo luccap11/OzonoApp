2bba892ef0c33e587b8b4d4307d396e9ff6cee40
Luca Capitoli <capitoli@skypointplanetariums.eu>
2021-01-11 17:52:57 +0100

    refactoring a string

--- app/src/main/java/it/luccap11/android/weatherconditions/model/RemoteWeatherDataSource.kt
+++ app/src/main/java/it/luccap11/android/weatherconditions/model/RemoteWeatherDataSource.kt
@@ -13,11 +13,11 @@ import org.json.JSONObject
  * @since 30/dec/2020
  */
 object RemoteWeatherDataSource {
-    private val baseUrl = "https://api.openweathermap.org/data/2.5/forecast"
+    private const val BASE_URL = "https://api.openweathermap.org/data/2.5/forecast"
 
     fun fetchOWeatherMapData(selectedCity: String, completion: (Resource<JSONObject>) -> Unit) {
         val resource = WeatherConditionApp.instance.resources
-        val url = String.format("%s?q=%s&APPID=%s&units=metric", baseUrl, selectedCity, resource.getString(R.string.owm_api_key))
+        val url = String.format("%s?q=%s&APPID=%s&units=metric", BASE_URL, selectedCity, resource.getString(R.string.owm_api_key))
         val requestQueue = Volley.newRequestQueue(WeatherConditionApp.instance)
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null, { response ->
             completion(Resource.Success(response))
