2ead127e52321cc00fb0034d5993e36fc1e06eda
Luca Capitoli <capitoli@skypointplanetariums.eu>
2021-01-27 12:52:19 +0100

    added method to retrieve world city data

--- app/src/main/java/it/luccap11/android/weatherconditions/model/WeatherViewModel.kt
+++ app/src/main/java/it/luccap11/android/weatherconditions/model/WeatherViewModel.kt
@@ -1,6 +1,10 @@
 package it.luccap11.android.weatherconditions.model
 
 import androidx.lifecycle.*
+import it.luccap11.android.weatherconditions.infrastructure.OWeatherMapRepository
+import it.luccap11.android.weatherconditions.infrastructure.Resource
+import it.luccap11.android.weatherconditions.infrastructure.WorldCitiesRepository
+import it.luccap11.android.weatherconditions.model.data.CityData
 import it.luccap11.android.weatherconditions.model.data.WeatherData
 import kotlinx.coroutines.Dispatchers
 import kotlinx.coroutines.launch
@@ -10,15 +14,27 @@ import kotlinx.coroutines.launch
  * @author Luca Capitoli
  */
 class WeatherViewModel : ViewModel() {
-    val liveData = MutableLiveData<Resource<List<WeatherData>>>()
-    private val repository = OWeatherMapRepository()
+    val weatherLiveData = MutableLiveData<Resource<List<WeatherData>>>()
+    val citiesLiveData = MutableLiveData<Resource<List<CityData>>>()
+    private val weatherRepository = OWeatherMapRepository()
+    private val cityRepository = WorldCitiesRepository()
 
     fun updateWeatherData(selectedCity: String) {
-        liveData.postValue(Resource.Loading())
+        weatherLiveData.postValue(Resource.Loading())
         viewModelScope.launch(Dispatchers.IO) {
-            repository.fetchWeatherData(selectedCity) { weatherData ->
-                liveData.postValue(weatherData)
+            weatherRepository.fetchWeatherData(selectedCity) { weatherData ->
+                weatherLiveData.postValue(weatherData)
             }
         }
     }
+
+    fun updateCityData(userQuery: String) {
+        citiesLiveData.postValue(Resource.Loading())
+        viewModelScope.launch(Dispatchers.IO) {
+            cityRepository.fetchCitiesData(userQuery) { worldCities ->
+                citiesLiveData.postValue(worldCities)
+            }
+        }
+    }
+
 }
