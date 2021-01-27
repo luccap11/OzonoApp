7b86c25980977f1202e9d9f1b989438d5d08fc8f
Luca Capitoli <capitoli@skypointplanetariums.eu>
2021-01-27 12:50:47 +0100

    first try with retrofit2

---
+++ app/src/main/java/it/luccap11/android/weatherconditions/infrastructure/RemoteWCitiesDataSource.kt
@@ -0,0 +1,49 @@
+package it.luccap11.android.weatherconditions.infrastructure
+
+import android.util.Log
+import it.luccap11.android.weatherconditions.infrastructure.RetrofitClientInstance.retrofitInstance
+import it.luccap11.android.weatherconditions.model.data.CityData
+import it.luccap11.android.weatherconditions.model.data.WorldCitiesData
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+import java.net.URLEncoder
+
+
+/**
+ * @author Luca Capitoli
+ * @since 13/jan/2021
+ */
+object RemoteWCitiesDataSource {
+
+    fun fetchBack4AppData(selectedCity: String, completion: (Resource<List<CityData>>) -> Unit) {
+        val service: WorldCitiesWS = retrofitInstance!!.create(WorldCitiesWS::class.java)
+        var where = URLEncoder.encode("""
+    {
+        "name": {
+            "${'$'}gte": "$selectedCity"
+        },
+        "country": {
+            "${'$'}exists": true
+        },
+        "population": {
+            "${'$'}gt": 1000
+        }
+    }
+    """.trimIndent(), "utf-8")
+        val call: Call<WorldCitiesData> = service.getCities(where)
+        call.enqueue(object : Callback<WorldCitiesData> {
+            override fun onResponse(call: Call<WorldCitiesData>, response: Response<WorldCitiesData>) {
+                val ok = response.isSuccessful
+                Log.d("AAA", response.message())
+                Log.e("AAA", response.errorBody().toString())
+                completion(Resource.Success(response.body()!!.cities.distinctBy { Pair(it.name, it.country.name) }.sortedByDescending { it.population }))
+            }
+
+            override fun onFailure(call: Call<WorldCitiesData>, t: Throwable?) {
+                Log.e("AAA", t.toString())
+                completion(Resource.Error(""))
+            }
+        })
+    }
+}
---
+++ app/src/main/java/it/luccap11/android/weatherconditions/infrastructure/WorldCitiesRepository.kt
@@ -0,0 +1,35 @@
+package it.luccap11.android.weatherconditions.infrastructure
+
+import it.luccap11.android.weatherconditions.R
+import it.luccap11.android.weatherconditions.WeatherConditionApp
+import it.luccap11.android.weatherconditions.model.data.*
+
+/**
+ * @author Luca Capitoli
+ * @since 13/jan/2021
+ */
+class WorldCitiesRepository {
+
+    fun fetchCitiesData(userQuery: String, completion: (Resource<List<CityData>>) -> Unit) {
+        val isCachedData = CitiesDataCache.isDataInCache(userQuery)
+        if (isCachedData) {
+            completion(Resource.Success(CitiesDataCache.getCachedCitiesData(userQuery)))
+        } else if (false) {
+            //check in db...
+        } else {
+            RemoteWCitiesDataSource.fetchBack4AppData(userQuery) { remoteResponse ->
+                when (remoteResponse) {
+                    is Resource.Success -> {
+//                        val result = WeatherDataParser.getWeatherLiveData()
+//                        CitiesDataCache.addCachedCityData(userQuery, remoteResponse)
+                        completion(Resource.Success(remoteResponse.data!!))
+                    }
+
+                    is Resource.Error -> {
+                        completion(Resource.Error(WeatherConditionApp.instance.resources.getString(R.string.error_label)))
+                    }
+                }
+            }
+        }
+    }
+}
