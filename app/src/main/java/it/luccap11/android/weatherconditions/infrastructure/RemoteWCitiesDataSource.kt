7285b31b362dcd2090c46ead23d18b2ef9627dc1
Luca Capitoli <capitoli@skypointplanetariums.eu>
2021-02-09 19:07:20 +0100

    refactoring: create new method that finds errors in response body

--- app/src/main/java/it/luccap11/android/weatherconditions/infrastructure/RemoteWCitiesDataSource.kt
+++ app/src/main/java/it/luccap11/android/weatherconditions/infrastructure/RemoteWCitiesDataSource.kt
@@ -36,14 +36,10 @@ object RemoteWCitiesDataSource {
         val call: Call<WorldCitiesData> = service.getCities(where)
         call.enqueue(object : Callback<WorldCitiesData> {
             override fun onResponse(call: Call<WorldCitiesData>, response: Response<WorldCitiesData>) {
-                val body = response.body()
-                if (body != null) {
-                    completion(Resource.Success(response.body()!!.cities))
-                } else{
-                    Log.e(WeatherConditionApp.instance.getString(R.string.error_log_tag), response.message())
-                    if (response.errorBody() != null)
-                        Log.e(WeatherConditionApp.instance.getString(R.string.error_log_tag), response.errorBody().toString())
+                if (findErrorsInBodyResponse(response, response.body())) {
                     completion(Resource.Error(""))
+                } else {
+                    completion(Resource.Success(response.body()!!.cities))
                 }
             }
 
@@ -51,5 +47,25 @@ object RemoteWCitiesDataSource {
                 completion(Resource.Error(""))
             }
         })
+    }
+
+    private fun findErrorsInBodyResponse(response: Response<WorldCitiesData>, body: WorldCitiesData?): Boolean {
+        if (body == null) {
+            Log.e(
+                WeatherConditionApp.instance.getString(R.string.error_log_tag),
+                response.message()
+            )
+
+            val errorBody = response.errorBody()
+            if (errorBody != null) {
+                Log.e(
+                    WeatherConditionApp.instance.getString(R.string.error_log_tag),
+                    errorBody.toString()
+                )
+            }
+            return true
+        } else {
+            return false
+        }
     }
 }
