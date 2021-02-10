604b97eddb14f29bb50a0071d784631ca4111c70
Luca Capitoli <capitoli@skypointplanetariums.eu>
2021-02-10 09:43:00 +0100

    updated androidx.test.ext:junit-ktx version and use AndroidJUnit4ClassRunner instead of deprecated class AndroidJUnit4

--- app/src/androidTest/java/it/luccap11/android/weatherconditions/MainFragmentTest.kt
+++ app/src/androidTest/java/it/luccap11/android/weatherconditions/MainFragmentTest.kt
@@ -7,13 +7,13 @@ import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
 import androidx.test.filters.LargeTest
-import androidx.test.runner.AndroidJUnit4
+import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import it.luccap11.android.weatherconditions.ui.main.MainFragment
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4::class)
+@RunWith(AndroidJUnit4ClassRunner::class)
 @LargeTest
 class MainFragmentTest {
     @Before
