7142762d4959cfb728e92d0a2bb3c7c4dc15b1c8
Luca Capitoli <capitoli@skypointplanetariums.eu>
2021-02-17 15:04:10 +0100

    filtered duplicate cities from API

--- app/src/main/java/it/luccap11/android/weatherconditions/infrastructure/RemoteWCitiesDataSource.kt
+++ app/src/main/java/it/luccap11/android/weatherconditions/infrastructure/RemoteWCitiesDataSource.kt
@@ -9,6 +9,8 @@ import it.luccap11.android.weatherconditions.model.data.WorldCitiesData
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
+import java.math.BigDecimal
+import java.math.RoundingMode
 import java.net.URLEncoder
 
 
@@ -17,6 +19,7 @@ import java.net.URLEncoder
  * @since 13/jan/2021
  */
 object RemoteWCitiesDataSource {
+    private const val THRESHOLD = 0.01
 
     fun fetchBack4AppData(selectedCity: String, completion: (Resource<List<CityData>>) -> Unit) {
         val service: WorldCitiesWS = retrofitInstance!!.create(WorldCitiesWS::class.java)
@@ -39,7 +42,8 @@ object RemoteWCitiesDataSource {
                 if (findErrorsInBodyResponse(response, response.body())) {
                     completion(Resource.Error(""))
                 } else {
-                    completion(Resource.Success(response.body()!!.cities))
+                    val filteredCities = filterDuplicates(response.body()!!.cities)
+                    completion(Resource.Success(filteredCities))
                 }
             }
 
@@ -50,22 +54,43 @@ object RemoteWCitiesDataSource {
     }
 
     private fun findErrorsInBodyResponse(response: Response<WorldCitiesData>, body: WorldCitiesData?): Boolean {
-        if (body == null) {
-            Log.e(
-                WeatherConditionApp.instance.getString(R.string.error_log_tag),
-                response.message()
-            )
+        return if (body == null) {
+            Log.e(WeatherConditionApp.instance.getString(R.string.error_log_tag), response.message())
 
             val errorBody = response.errorBody()
             if (errorBody != null) {
-                Log.e(
-                    WeatherConditionApp.instance.getString(R.string.error_log_tag),
-                    errorBody.toString()
-                )
+                Log.e(WeatherConditionApp.instance.getString(R.string.error_log_tag), errorBody.toString())
             }
-            return true
+            true
         } else {
-            return false
+            false
         }
+    }
+
+    private fun filterDuplicates(remoteCities: List<CityData>): List<CityData> {
+        val orderedRemoteCities = remoteCities.sortedByDescending { it.population }
+        val uniqueCities = mutableSetOf<CityData>()
+        orderedRemoteCities.forEach { cityData ->
+            var insertInSet = true
+            uniqueCities.forEach { uniqueCity ->
+                val latitMaxThreshold = BigDecimal(uniqueCity.location.latitude.toDouble() + THRESHOLD).setScale(2, RoundingMode.HALF_EVEN)
+                val latitMinThreshold = BigDecimal(uniqueCity.location.latitude.toDouble() - THRESHOLD).setScale(2, RoundingMode.HALF_EVEN)
+                val longitMaxThreshold = BigDecimal(uniqueCity.location.longitude.toDouble() + THRESHOLD).setScale(2, RoundingMode.HALF_EVEN)
+                val longitMinThreshold = BigDecimal(uniqueCity.location.longitude.toDouble() - THRESHOLD).setScale(2, RoundingMode.HALF_EVEN)
+                val currentLatitRound = BigDecimal(cityData.location.latitude.toDouble()).setScale(2, RoundingMode.HALF_EVEN)
+                val currentLongitRound = BigDecimal(cityData.location.longitude.toDouble()).setScale(2, RoundingMode.HALF_EVEN)
+
+                if (cityData.name.equals(uniqueCity.name, true) && cityData.country.name.equals(uniqueCity.country.name, true) &&
+                    (currentLatitRound in latitMinThreshold..latitMaxThreshold && currentLongitRound in longitMinThreshold..longitMaxThreshold) &&
+                    cityData.population <= uniqueCity.population) {
+                    insertInSet = false
+                }
+            }
+            if (insertInSet) {
+                uniqueCities.add(cityData)
+            }
+        }
+
+        return uniqueCities.toList()
     }
 }
