e176dedb8b0cd1c4b6ef67623580040986af2a96
Luca Capitoli <capitoli@skypointplanetariums.eu>
2021-03-05 12:40:17 +0100

    fixed ui tests

--- app/src/androidTest/java/it/luccap11/android/weatherconditions/MainFragmentTest.kt
+++ app/src/androidTest/java/it/luccap11/android/weatherconditions/MainFragmentTest.kt
@@ -1,32 +1,38 @@
 package it.luccap11.android.weatherconditions
 
 import android.view.KeyEvent
-import androidx.fragment.app.testing.launchFragmentInContainer
+import android.view.View
+import androidx.appcompat.widget.SearchView
+import androidx.fragment.app.testing.FragmentScenario.Companion.launchInContainer
 import androidx.test.espresso.Espresso.onView
+import androidx.test.espresso.UiController
+import androidx.test.espresso.ViewAction
 import androidx.test.espresso.action.ViewActions.*
 import androidx.test.espresso.assertion.ViewAssertions.matches
 import androidx.test.espresso.matcher.ViewMatchers.*
+import androidx.test.ext.junit.runners.AndroidJUnit4
 import androidx.test.filters.LargeTest
-import androidx.test.internal.runner.junit4.AndroidJUnit4ClassRunner
 import it.luccap11.android.weatherconditions.ui.main.MainFragment
+import org.hamcrest.Matcher
+import org.hamcrest.core.AllOf.allOf
 import org.junit.Before
 import org.junit.Test
 import org.junit.runner.RunWith
 
-@RunWith(AndroidJUnit4ClassRunner::class)
+@RunWith(AndroidJUnit4::class)
 @LargeTest
 class MainFragmentTest {
     @Before
     fun before() {
-        launchFragmentInContainer<MainFragment>()
+        launchInContainer(MainFragment::class.java, null, R.style.Theme_AppCompat, null)
     }
 
     @Test
     fun checkAllViews() {
         onView(withId(R.id.searchView)).check(matches(isDisplayed()))
-        onView(withId(R.id.progressLoading))
+        onView(withId(R.id.weatherDataLoading))
         onView(withId(R.id.citiesList))
-        onView(withId(R.id.text))
+        onView(withId(R.id.weatherMessage))
         onView(withId(R.id.listWeatherData))
     }
 
@@ -34,6 +40,22 @@ class MainFragmentTest {
     fun checkSearchView() {
         onView(withId(R.id.searchView)).check(matches(isDisplayed()))
         onView(withId(R.id.searchView)).perform(click())
-        onView(withId(R.id.searchView)).perform(typeText("London"), pressKey(KeyEvent.KEYCODE_ENTER))
+        onView(withId(R.id.searchView)).perform(typeSearchViewText("London"), pressKey(KeyEvent.KEYCODE_ENTER))
+    }
+
+    fun typeSearchViewText(text: String): ViewAction {
+        return object : ViewAction {
+            override fun getDescription(): String {
+                return "Change view text"
+            }
+
+            override fun getConstraints(): Matcher<View> {
+                return allOf(isDisplayed(), isAssignableFrom(SearchView::class.java))
+            }
+
+            override fun perform(uiController: UiController?, view: View?) {
+                (view as SearchView).setQuery(text, false)
+            }
+        }
     }
 }
