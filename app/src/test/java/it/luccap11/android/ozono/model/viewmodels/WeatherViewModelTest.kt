c2b7dbbae73a70fb16b4785e45d2ba629dcb7511
Luca Capitoli <capitoli@skypointplanetariums.eu>
2021-06-18 12:15:40 +0200

    github problems

--- app/src/test/java/it/luccap11/android/ozono/model/viewmodels/WeatherViewModelTest.kt
+++ app/src/test/java/it/luccap11/android/ozono/model/viewmodels/WeatherViewModelTest.kt
@@ -50,7 +50,7 @@ class WeatherViewModelTest {
         }
 
     @Test
-//    @Ignore("Github launches a TimeoutException")
+    @Ignore("Github launches a TimeoutException")
     fun updateWeatherDataTest_liveData_error() = coroutinesTestRule.testDispatcher.runBlockingTest {
         `when`(weatherRepository.fetchWeatherDataByCityName(anyString()))
             .thenReturn(null)
